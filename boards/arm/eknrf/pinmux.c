f7142177e1f9ca09677136c6490abe701a67b569
ergokeeb <patwyh_reg@yahoo.com.hk>
2026-03-09 00:10:13 +0800

    eknrf

---
+++ boards/arm/eknrf/pinmux.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/sys_io.h>
+#include <zephyr/devicetree.h>
+
+static int pinmux_eknrf_init(void) {
+//#if CONFIG_BOARD_EKNRF
+// Remove or comment out the block targeting pin 5
+    const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+    return 0;
+/* 
+#if CONFIG_BOARD_EKNRF_CHARGER
+    gpio_pin_configure(p0, 5, GPIO_OUTPUT);
+    gpio_pin_set(p0, 5, 0);
+#else
+    gpio_pin_configure(p0, 5, GPIO_INPUT);
+#endif
+   
+#endif
+    return 0;*/
+} 
+
+SYS_INIT(pinmux_eknrf_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
